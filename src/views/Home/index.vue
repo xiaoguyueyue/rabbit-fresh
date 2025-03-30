15034c5fb1babc6665ef55f851d646a11101b6eb
i_xiaobiaoxu <i_xiaobiaoxu@cvte.com>
2025-03-30 19:49:58 +0800

    第二天

---
+++ src/views/Home/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div style="height: 1200px;">我是Home</div>
+</template>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import LayoutFixed from './components/LayoutFixed.vue'
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFoooter from './components/LayoutFoooter.vue'
+import {useCategoryStore} from '@/stores/category'
+import { onMounted } from 'vue'
+const categoryStore = useCategoryStore()
+onMounted(() =>{
+  categoryStore.getCategory()
+})
+</script>
+
+<template>
+  <LayoutFixed/>
+  <LayoutNav/>
+  <LayoutHeader/>
+  <RouterView/>
+  <LayoutFoooter/>
+</template>
