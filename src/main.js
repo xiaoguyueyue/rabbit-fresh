ec501509f90ae6c3b923487b48f01bb182a682b7
xiaobiaoxu <1014806711@qq.com>
2025-05-18 15:15:43 +0800

    第三天

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { lazyPlugin } from '@/directives'
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
--- src/stores/category.js
+++ src/stores/category.js
@@ -3,15 +3,13 @@ import { defineStore } from 'pinia'
 import { getCategoryAPI } from '@/apis/layout'
 
 export const useCategoryStore = defineStore('category', () => {
-
-const categoryList = ref([])
-const getCategory = async ()=>{
-  const res = await getCategoryAPI()
-  console.log(res);
-  categoryList.value = res.result
-}
-return {
-  categoryList,
-  getCategory
-}
+  const categoryList = ref([])
+  const getCategory = async ()=>{
+    const res = await getCategoryAPI()
+    categoryList.value = res.result
+  }
+  return {
+    categoryList,
+    getCategory
+  }
 })
